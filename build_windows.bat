33a36b42c78c9dc49f24432d0f24322b420ce9b3
Tarcio Diniz <tarciodiniz0@gmail.com>
2023-06-25 12:06:29 -0300

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -5,8 +5,8 @@ set COMPILER_COMMAND=gcc
 set COMPILER_FLAGS=-std=c11 -Wall
 set OUTPUT_EXECUTABLE=gerador_sudoku.exe
 
-rem Compilação do programa
-%COMPILER_COMMAND% %COMPILER_FLAGS% -Iinclude src/criador.c src/solucionador.c src/historico.c src/game_dao.c main.c -o %OUTPUT_EXECUTABLE%
+rem Compilação dos arquivos
+%COMPILER_COMMAND% %COMPILER_FLAGS% -Iinclude -o %OUTPUT_EXECUTABLE% src/criador.c src/solucionador.c src/historico.c src/game_dao.c main.c
 
 rem Execução do programa
 .\%OUTPUT_EXECUTABLE%
